d09595a2518b6dbef31c2d0e4e7841ea43b94c65
el10savio <el10savio96@gmail.com>
2021-11-15 19:25:52 +0530

    Added List Traversal Functions

--- linkedList/linkedList.go
+++ linkedList/linkedList.go
@@ -1,8 +1,10 @@
 package linkedlist
 
+import "errors"
+
 // Node ...
 type Node struct {
-	Value    interface{}
+	Value    int
 	previous *Node
 	next     *Node
 }
@@ -11,9 +13,39 @@ type Node struct {
 type DoublyLinkedList struct {
 	Length uint
 	head   *Node
+	tail   *Node
 }
 
 // InitDoublyLinkedList ...
 func InitDoublyLinkedList(length uint) *DoublyLinkedList {
-	return &DoublyLinkedList{Length: length, head: nil}
+	return &DoublyLinkedList{Length: length, head: nil, tail: nil}
+}
+
+// List ...
+func (dList *DoublyLinkedList) List() []int {
+	return traverseList(dList.head)
+}
+
+// traverseList ...
+func traverseList(head *Node) []int {
+	if head == nil {
+		return []int{}
+	}
+	return append([]int{head.Value}, traverseList(head.next)...)
+}
+
+// GetHead ...
+func (dlist *DoublyLinkedList) GetHead() (int, error) {
+	if dlist.head == nil {
+		return 0, errors.New("head does not exist")
+	}
+	return dlist.head.Value, nil
+}
+
+// GetTail ...
+func (dlist *DoublyLinkedList) GetTail() (int, error) {
+	if dlist.tail == nil {
+		return 0, errors.New("tail does not exist")
+	}
+	return dlist.tail.Value, nil
 }
