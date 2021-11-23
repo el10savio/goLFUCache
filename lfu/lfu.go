ac83a13e40453bc05c4dbe4051d60878125a00b1
el10savio <el10savio96@gmail.com>
2021-11-23 22:17:37 +0530

    Init LFU struct

--- lfu/lfu.go
+++ lfu/lfu.go
@@ -1,1 +1,24 @@
 package lfu
+
+import (
+	linkedList "github.com/el10savio/goLFUCache/linkedList"
+)
+
+// LFU ...
+type LFU struct {
+	Capacity      uint
+	hashTable     map[int]int
+	frequencyList linkedList.DoublyLinkedList
+	accessList    linkedList.DoublyLinkedList
+}
+
+// InitLFU ...
+// TODO: err check if capacity is 0
+func InitLFU(capacity uint) *LFU {
+	return &LFU{
+		Capacity:      capacity,
+		hashTable:     make(map[int]int),
+		frequencyList: *linkedList.InitDoublyLinkedList(),
+		accessList:    *linkedList.InitDoublyLinkedList(),
+	}
+}
