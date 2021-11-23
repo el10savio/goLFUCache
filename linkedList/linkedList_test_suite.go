feeed1becc61b7e39a1756bf45f540ccad8eeefb
el10savio <el10savio96@gmail.com>
2021-11-23 14:22:01 +0530

    Refactored LinkedList Test Suite Errors

--- linkedList/linkedList_test_suite.go
+++ linkedList/linkedList_test_suite.go
@@ -1,7 +1,5 @@
 package linkedlist
 
-import "errors"
-
 var testInitDoublyLinkedListTestSuite = []struct {
 	name                     string
 	expectedDoublyLinkedList *DoublyLinkedList
@@ -47,15 +45,15 @@ var testInsertBetweenTestSuite = []struct {
 }{
 	{"BasicFuntionality", []int{1, 2, 3, 4, 5}, 7, 3, 4, []int{1, 2, 3, 7, 4, 5}, []int{5, 4, 7, 3, 2, 1}, nil},
 	{"TwoElements", []int{1, 2}, 7, 1, 2, []int{1, 7, 2}, []int{2, 7, 1}, nil},
-	{"EmptyList", []int{}, 7, 3, 4, []int{}, []int{}, errors.New("head does not exist")},
-	{"SingleElement", []int{1}, 7, 1, 4, []int{1}, []int{1}, errors.New("range does not exist")},
-	{"RangeDoesNotExist", []int{1, 2, 3, 4, 5}, 7, 11, 21, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errors.New("range does not exist")},
-	{"RangeStartDoesNotExist", []int{1, 2, 3, 4, 5}, 7, 11, 4, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errors.New("range does not exist")},
-	{"RangeEndDoesNotExist", []int{1, 2, 3, 4, 5}, 7, 3, 21, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errors.New("range does not exist")},
+	{"EmptyList", []int{}, 7, 3, 4, []int{}, []int{}, errHeadDoesNotExist},
+	{"SingleElement", []int{1}, 7, 1, 4, []int{1}, []int{1}, errRangeDoesNotExist},
+	{"RangeDoesNotExist", []int{1, 2, 3, 4, 5}, 7, 11, 21, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errRangeDoesNotExist},
+	{"RangeStartDoesNotExist", []int{1, 2, 3, 4, 5}, 7, 11, 4, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errRangeDoesNotExist},
+	{"RangeEndDoesNotExist", []int{1, 2, 3, 4, 5}, 7, 3, 21, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errRangeDoesNotExist},
 	{"RangeValuesSameAsInsert", []int{1, 2, 3, 3, 5}, 3, 3, 3, []int{1, 2, 3, 3, 3, 5}, []int{5, 3, 3, 3, 2, 1}, nil},
 	{"RangeStartSameAsInsert", []int{1, 2, 3, 4, 5}, 3, 3, 4, []int{1, 2, 3, 3, 4, 5}, []int{5, 4, 3, 3, 2, 1}, nil},
 	{"RangeEndSameAsInsert", []int{1, 2, 3, 4, 5}, 4, 3, 4, []int{1, 2, 3, 4, 4, 5}, []int{5, 4, 4, 3, 2, 1}, nil},
-	{"RangeNotInBetween", []int{1, 2, 3, 4, 5}, 7, 3, 5, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errors.New("range does not exist")},
+	{"RangeNotInBetween", []int{1, 2, 3, 4, 5}, 7, 3, 5, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errRangeDoesNotExist},
 	{"RangeInBetweenEnd", []int{1, 2, 3, 4, 5}, 7, 4, 5, []int{1, 2, 3, 4, 7, 5}, []int{5, 7, 4, 3, 2, 1}, nil},
 }
 
@@ -66,7 +64,7 @@ var testGetHeadTestSuite = []struct {
 	expectedError error
 }{
 	{"BasicFuntionality", []int{10}, 10, nil},
-	{"EmptyList", []int{}, 0, errors.New("head does not exist")},
+	{"EmptyList", []int{}, 0, errHeadDoesNotExist},
 	{"TwoValues", []int{1, 2}, 1, nil},
 	{"MultipleValues", []int{1, 2, 3, 4, 5}, 1, nil},
 }
@@ -78,7 +76,7 @@ var testGetTailTestSuite = []struct {
 	expectedError error
 }{
 	{"BasicFuntionality", []int{10}, 10, nil},
-	{"EmptyList", []int{}, 0, errors.New("tail does not exist")},
+	{"EmptyList", []int{}, 0, errTailDoesNotExist},
 	{"TwoValues", []int{1, 2}, 2, nil},
 	{"MultipleValues", []int{1, 2, 3, 4, 5}, 5, nil},
 }
@@ -91,7 +89,7 @@ var testRemoveHeadTestSuite = []struct {
 	expectedError           error
 }{
 	{"BasicFuntionality", []int{10}, []int{}, []int{}, nil},
-	{"EmptyValues", []int{}, []int{}, []int{}, errors.New("head does not exist")},
+	{"EmptyValues", []int{}, []int{}, []int{}, errHeadDoesNotExist},
 	{"SizeOne", []int{10}, []int{}, []int{}, nil},
 	{"TwoValues", []int{1, 2}, []int{2}, []int{2}, nil},
 	{"MultipleValues", []int{1, 2, 3, 4, 5}, []int{2, 3, 4, 5}, []int{5, 4, 3, 2}, nil},
@@ -105,7 +103,7 @@ var testRemoveTailTestSuite = []struct {
 	expectedError           error
 }{
 	{"BasicFuntionality", []int{10}, []int{}, []int{}, nil},
-	{"EmptyValues", []int{}, []int{}, []int{}, errors.New("tail does not exist")},
+	{"EmptyValues", []int{}, []int{}, []int{}, errTailDoesNotExist},
 	{"SizeOne", []int{10}, []int{}, []int{}, nil},
 	{"TwoValues", []int{1, 2}, []int{1}, []int{1}, nil},
 	{"MultipleValues", []int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4}, []int{4, 3, 2, 1}, nil},
@@ -123,8 +121,8 @@ var testRemoveElementTestSuite = []struct {
 	{"SingleElement", []int{1}, 1, []int{}, []int{}, nil},
 	{"TwoElementsValueAtStart", []int{1, 2}, 1, []int{2}, []int{2}, nil},
 	{"TwoElementsValueAtEnd", []int{1, 2}, 2, []int{1}, []int{1}, nil},
-	{"EmptyList", []int{}, 3, []int{}, []int{}, errors.New("head does not exist")},
-	{"ValueDoesNotExist", []int{1, 2, 3, 4, 5}, 7, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errors.New("element does not exist")},
+	{"EmptyList", []int{}, 3, []int{}, []int{}, errHeadDoesNotExist},
+	{"ValueDoesNotExist", []int{1, 2, 3, 4, 5}, 7, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}, errElementDoesNotExist},
 	{"MultipleValuesExist", []int{1, 3, 3, 4, 5}, 3, []int{1, 3, 4, 5}, []int{5, 4, 3, 1}, nil},
 	{"ValueExistsAtStart", []int{1, 2, 3, 4, 5}, 1, []int{2, 3, 4, 5}, []int{5, 4, 3, 2}, nil},
 	{"ValueExistsAtEnd", []int{1, 2, 3, 4, 5}, 5, []int{1, 2, 3, 4}, []int{4, 3, 2, 1}, nil},
@@ -140,8 +138,8 @@ var testFindElementTestSuite = []struct {
 	{"BasicFunctionality", []int{1, 2, 3, 4, 5}, 3, 3, nil},
 	{"SingleElement", []int{1}, 1, 1, nil},
 	{"TwoElements", []int{1, 2}, 2, 2, nil},
-	{"EmptyList", []int{}, 11, 0, errors.New("head does not exist")},
-	{"NodeNotPresent", []int{1, 2, 3, 4, 5}, 11, 0, errors.New("element does not exist")},
+	{"EmptyList", []int{}, 11, 0, errHeadDoesNotExist},
+	{"NodeNotPresent", []int{1, 2, 3, 4, 5}, 11, 0, errElementDoesNotExist},
 }
 
 var testGetNextElementTestSuite = []struct {
@@ -152,11 +150,11 @@ var testGetNextElementTestSuite = []struct {
 	expectedError       error
 }{
 	{"BasicFunctionality", []int{1, 2, 3, 4, 5}, 3, 4, nil},
-	{"SingleElement", []int{1}, 1, 0, errors.New("next element does not exist")},
+	{"SingleElement", []int{1}, 1, 0, errNextElementDoesNotExist},
 	{"TwoElements", []int{1, 2}, 1, 2, nil},
-	{"EmptyList", []int{}, 11, 0, errors.New("element does not exist")},
-	{"NodeNotPresent", []int{1, 2, 3, 4, 5}, 11, 0, errors.New("element does not exist")},
-	{"NextNodeNotPresent", []int{1, 2, 3, 4, 5}, 5, 0, errors.New("next element does not exist")},
+	{"EmptyList", []int{}, 11, 0, errElementDoesNotExist},
+	{"NodeNotPresent", []int{1, 2, 3, 4, 5}, 11, 0, errElementDoesNotExist},
+	{"NextNodeNotPresent", []int{1, 2, 3, 4, 5}, 5, 0, errNextElementDoesNotExist},
 }
 
 var testGetPreviousElementTestSuite = []struct {
@@ -167,9 +165,9 @@ var testGetPreviousElementTestSuite = []struct {
 	expectedError           error
 }{
 	{"BasicFunctionality", []int{1, 2, 3, 4, 5}, 3, 2, nil},
-	{"SingleElement", []int{1}, 1, 0, errors.New("previous element does not exist")},
+	{"SingleElement", []int{1}, 1, 0, errPreviousElementDoesNotExist},
 	{"TwoElements", []int{1, 2}, 2, 1, nil},
-	{"EmptyList", []int{}, 11, 0, errors.New("element does not exist")},
-	{"NodeNotPresent", []int{1, 2, 3, 4, 5}, 11, 0, errors.New("element does not exist")},
-	{"PreviousNodeNotPresent", []int{1, 2, 3, 4, 5}, 1, 0, errors.New("previous element does not exist")},
+	{"EmptyList", []int{}, 11, 0, errElementDoesNotExist},
+	{"NodeNotPresent", []int{1, 2, 3, 4, 5}, 11, 0, errElementDoesNotExist},
+	{"PreviousNodeNotPresent", []int{1, 2, 3, 4, 5}, 1, 0, errPreviousElementDoesNotExist},
 }
